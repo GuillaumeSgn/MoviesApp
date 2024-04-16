d2e1e7a5cb74f48b2d92d0cfc6f2cd081fd540a2
Guillaume Seguin <guillaume.seguin@worldline.com>
2024-04-16 16:54:30 +0200

    review

--- presentation/src/main/java/com/example/presentation/home/ListMovies.kt
+++ presentation/src/main/java/com/example/presentation/home/ListMovies.kt
@@ -1,4 +1,4 @@
-package com.example.presentation
+package com.example.presentation.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
--- presentation/src/main/java/com/example/presentation/home/Navigation.kt
+++ presentation/src/main/java/com/example/presentation/home/Navigation.kt
@@ -1,4 +1,4 @@
-package com.example.presentation
+package com.example.presentation.home
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavType
